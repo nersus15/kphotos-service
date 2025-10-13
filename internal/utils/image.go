c9c9a707d2de6b4fdc05ebe4b7886ff46148fb09
Fathurrahman <fathur.ashter15@gmail.com>
2025-10-13 09:46:10 +0800

    UPDATE: ganti ganti bimg → github.com/disintegration/imaging

--- internal/utils/image.go
+++ internal/utils/image.go
@@ -1,19 +1,43 @@
 package utils
 
 import (
-	"github.com/h2non/bimg"
+	"image"
+	"log"
+	"os"
+
+	"github.com/disintegration/imaging"
 )
 
+// GenerateThumbnail membuat thumbnail 320px (lebar) dari input ke output
 func GenerateThumbnail(inputPath, outputPath string) error {
-	buffer, err := bimg.Read(inputPath)
+	src, err := imaging.Open(inputPath)
 	if err != nil {
 		return err
 	}
 
-	newImage, err := bimg.NewImage(buffer).Resize(320, 0)
+	thumb := imaging.Resize(src, 320, 0, imaging.Lanczos)
+
+	err = imaging.Save(thumb, outputPath)
 	if err != nil {
 		return err
 	}
 
-	return bimg.Write(outputPath, newImage)
+	return nil
+}
+
+// Optional: fungsi cek dimensi (bisa dipakai di upload handler)
+func GetImageSize(path string) (width, height int) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Println("get size:", err)
+		return 0, 0
+	}
+	defer file.Close()
+
+	img, _, err := image.DecodeConfig(file)
+	if err != nil {
+		log.Println("decode size:", err)
+		return 0, 0
+	}
+	return img.Width, img.Height
 }
